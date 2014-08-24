a5ee8a526e84030b16eb23dcc8ff66728f049690
Chase Adams <chase@realchaseadams.com>
2014-08-23 17:33:13 -0700

    Initial commit of golang json api server for two endpoints for core values

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import(
+"fmt"
+"log"
+"net/http"
+"encoding/json"
+"io/ioutil"
+"github.com/bmizerany/pat"
+)
+
+type CoreValueCollection struct {
+  JobsUrl string `json:"jobsUrl"`
+  CoreValues map[string]CoreValue `json:"values"`
+}
+
+type CoreValue struct {
+  Name string `json:"name"`
+  Id string `json:"id"`
+  Description string `json:"description"`
+  Summary string `json:"summary"`
+}
+
+var values CoreValueCollection
+
+func main() {
+  loadCoreValues()
+  mux := pat.New()
+  mux.Get("/CoreValue", http.HandlerFunc(rootHandler))
+  mux.Get("/CoreValue/:id", http.HandlerFunc(valueHandler))
+
+  http.Handle("/", mux)
+  log.Fatal(http.ListenAndServe(":8080", nil))
+  fmt.Println("Server started at http://localhost:8080")
+
+}
+
+func loadCoreValues() {
+  doc, err := ioutil.ReadFile("./CoreValues.json")
+
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  err = json.Unmarshal(doc, &values)
+
+  return
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+  response, _ := json.Marshal(values)
+  w.Header().Set("Content-Type", "application/json")
+  w.Write(response)
+}
+
+func valueHandler(w http.ResponseWriter, r *http.Request) {
+  params := r.URL.Query()
+  id := params.Get(":id")
+  w.Header().Set("Content-Type", "application/json")
+  response, _ := json.Marshal(values.CoreValues[id])
+  w.Write(response)
+}
